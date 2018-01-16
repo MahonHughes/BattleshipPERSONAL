7930dca836f9c8e11bb70c3a6d418a8e81a46167
MahonHughes <mahonhughes@gmail.com>
2018-01-16 23:57:24 +0000

    Added buildAll function

--- BattleshipsPERSONAL/BattleshipsPERSONAL/Program.cs
+++ BattleshipsPERSONAL/BattleshipsPERSONAL/Program.cs
@@ -33,12 +33,95 @@ namespace Test_of_increace
             return Number;
         }
 
-        static void BuildAll()
+        static void addShips(int size, string[,] hitMatrix, string[,] selectMatrix)
         {
-            int size = 0;
-            string input = Console.ReadLine();
-            size = Convert.ToInt32(input);
-            if (size == 0)
+            //size = safeIntInput("Enter Size: ");
+            //selectMatrix[0, 0] = "X";
+            int[] currentSelection = new int[2];
+            currentSelection[0] = 5;
+            currentSelection[1] = 5;
+            while (true)
+            {
+
+                selectMatrix[currentSelection[0], currentSelection[1]] = "X";
+
+                BuildAll(12, hitMatrix, selectMatrix);
+                ConsoleKeyInfo keyinfo;
+                Console.WriteLine(currentSelection[0]);
+                Console.WriteLine(currentSelection[1]);
+                keyinfo = Console.ReadKey();
+                //Console.WriteLine(keyinfo.Key);
+                if (keyinfo.Key == ConsoleKey.UpArrow)
+                {
+                    //Console.WriteLine("Up");
+                    if (currentSelection[1] >= 1){
+                        currentSelection[1] = currentSelection[1] - 1;
+                    }
+                }
+                else if (keyinfo.Key == ConsoleKey.DownArrow)
+                {
+                    Console.WriteLine("Down");
+                    if (currentSelection[1] < size - 1)
+                    {
+                        currentSelection[1] = currentSelection[1] + 1;
+                    }
+                }
+                else if (keyinfo.Key == ConsoleKey.LeftArrow)
+                {
+                    Console.WriteLine("Left");
+                    if (currentSelection[0] > 0)
+                    {
+                        currentSelection[0] = currentSelection[1] - 1;
+                    }
+                }
+                else if (keyinfo.Key == ConsoleKey.RightArrow)
+                {
+                    Console.WriteLine("Right");
+                    if (currentSelection[0] < size - 1)
+                    {
+                        currentSelection[0] = currentSelection[0] + 1;
+                    }
+                }
+
+
+                for (int i = 0; i <= 11; i++)
+                {
+                    for (int j = 0; j <= 11; j++)
+                    {
+                        selectMatrix[i, j] = "O";
+                    }
+                }
+
+            }
+
+        }
+
+        static void BuildAll(int size, string[,] hitMatrix, string[,] selectMatrix)
+        {
+            ConsoleColor[,] textColorMatrix = new ConsoleColor[size, size];
+            ConsoleColor[,] backColorMatrix = new ConsoleColor[size, size];
+
+            for (int i = 0; i <= 11; i++)
+            {
+                for (int j = 0; j <= 11; j++)
+                {
+                    textColorMatrix[i, j] = ConsoleColor.White;
+                    backColorMatrix[i, j] = ConsoleColor.Black;
+                }
+            }
+            for (int i = 0; i <= 11; i++)
+            {
+                for (int j = 0; j <= 11; j++)
+                {
+                    if (selectMatrix[i, j] == "X")
+                    {
+                        textColorMatrix[i, j] = ConsoleColor.Blue;
+                        backColorMatrix[i, j] = ConsoleColor.White;
+                    }
+                }
+            }
+
+                    if (size == 0)
             {
                 Console.WriteLine("Excuse me?");
                 System.Threading.Thread.Sleep(3000);
